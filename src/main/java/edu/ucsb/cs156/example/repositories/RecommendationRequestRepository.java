9a80bf5c15a26dbe7375f04ca23e46f670013734
Wenxuan Xu <xwx@169-231-90-31.wireless.ucsb.edu>
2023-07-28 10:54:20 -0700

    fixed table for RecRequest

--- src/main/java/edu/ucsb/cs156/example/repositories/RecommendationRequestRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/RecommendationRequestRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RecommendationRequestRepository extends CrudRepository<RecommendationRequest, String> {
- 
+public interface RecommendationRequestRepository extends CrudRepository<RecommendationRequest, Long> {
+    
 }
