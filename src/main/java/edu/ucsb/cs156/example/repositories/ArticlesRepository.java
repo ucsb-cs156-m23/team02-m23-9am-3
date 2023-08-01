8cef83962584e238b8d0431aa80de5e41ae08b0a
Danny You <kyou@ucsb.edu>
2023-07-31 22:53:01 -0700

    Added Entity and Repository files for Articles

--- src/main/java/edu/ucsb/cs156/example/repositories/ArticlesRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/ArticlesRepository.java
@@ -5,7 +5,6 @@ import edu.ucsb.cs156.example.entities.Articles;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface ArticlesRepository extends CrudRepository<Articles, Long> {
   
